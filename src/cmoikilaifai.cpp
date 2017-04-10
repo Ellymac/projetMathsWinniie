c97a3446c3ce6e5f5e2b5297b15f2cfbeb646ef4
PERRY Fabien <fabien.perry@yahoo.fr>
2017-04-10 15:17:03 +0200

    cc

--- src/cmoikilaifai.cpp
+++ src/cmoikilaifai.cpp
@@ -2,7 +2,13 @@ void conservatives(real* Y, real* W){
 	W = Y;
 	int i;
 	for(i = 1; i <= 4; i ++){
-		W[i] = W[0] * W[i];
+		if(i == 2){
+			W[i] = Y[2]/(gam-1) + Y[0]*(Y[1]*Y[1]+Y[3]*Y[3]+Y[4]*Y[4])/2 + (Y[7]*Y[7]+Y[5]*Y[5]+Y[6]*Y[6])/2;
+		}
+		else{
+			W[i] = W[0] * W[i];
+		}
+		
 	}
 }
 
@@ -10,6 +16,11 @@ void primitives(real* Y, real* W){
 	Y = W;
 	int i;
 	for(i = 1; i <= 4; i ++){
-		Y[i] = Y[i] / Y[0];
+		if(i == 2){
+			Y[i] = (gam-1)*(W[i] - W[0]*(W[1]*W[1]+W[3]*W[3]/(Y[0]*Y[0])+W[4]*W[4]/(Y[0]*Y[0]))/2 + (Y[7]*Y[7]+Y[5]*Y[5]+Y[6]*Y[6]));
+		}
+		else{
+			Y[i] = Y[i] / Y[0];
+		}
 	}
 }
